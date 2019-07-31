e33af5871763a9a20a8b4db365496663fd844a56
Lucas Domingues <lucasdomi@globo.com>
2019-07-30 23:11:06 -0300

    fix: trocando div pai por fragment

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Header from "./components/Header";
 import PostList from "./components/PostList";
 import "./App.css";
 
 function App() {
   return (
-    <div>
+    <Fragment>
       <Header />
       <PostList />
-    </div>
+    </Fragment>
   );
 }
 
