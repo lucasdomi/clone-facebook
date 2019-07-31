05a64ee323d69a3a90aafc0f98efe37e1b30804b
Lucas Domingues <lucasdomi@globo.com>
2019-07-30 22:39:50 -0300

    fix: ajustando posicao do header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,8 +4,10 @@ import "./Header.css";
 function Header() {
   return (
     <div className="header">
-      <h1 className="title facebook">facebook</h1>
-      <h4 className="title perfil">meu perfil</h4>
+      <div className="text-header">
+        <h1 className="title facebook">facebook</h1>
+        <h4 className="title perfil">meu perfil</h4>
+      </div>
     </div>
   );
 }
